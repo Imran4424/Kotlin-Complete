f60132cd81d4d830b304d7f04eea9d3f2501d0a2
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-08 01:37:32 +0600

    conditional logic

---
+++ 3. Conditional Logic/9.When Digit.kt
@@ -0,0 +1,54 @@
+
+
+fun main(args: Array<String>) {
+    var digit = 7
+
+    when (digit) {
+        0 -> {
+            println("Zero")
+            println("First of Decimal Digit")
+        }
+        1 -> {
+            println("One")
+            println("Second of Decimal Digit")
+        }
+        2 -> {
+            println("Two")
+            println("Third of Decimal Digit")
+        }
+        3 -> {
+            println("Three")
+            println("Fourth of Decimal Digit")
+        }
+        4 -> {
+            println("Four")
+            println("Fifth of Decimal Digit")
+        }
+        5 -> {
+            println("Five")
+            println("Six-th of Decimal Digit")
+        }
+        6 -> {
+            println("Six")
+            println("Seven-th of Decimal Digit")
+        }
+        7 -> {
+            println("Seven")
+            println("Eight-th of Decimal Digit")
+        }
+        8 -> {
+            println("Eight")
+            println("Nine-th of Decimal Digit")
+        }
+        9 -> {
+            println("Nine")
+            println("Ten-th of Decimal Digit")
+        }
+        else -> {
+            println("Not a digit")
+            println("Not a decimal digit")
+        }
+    }
+
+
+}
