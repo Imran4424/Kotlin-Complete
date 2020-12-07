de346cfe9776920f9d45be91fc15701a76cf8d97
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-08 00:45:03 +0600

    operator

---
+++ 3. Conditional Logic/2.IF.kt
@@ -0,0 +1,11 @@
+
+
+fun main(args: Array<String>) {
+    val number: Int = 7
+
+    if (number % 2 == 0) {
+        println("$number is even")
+    } else {
+        println("$number is odd")
+    }
+}
