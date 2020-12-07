602f3ba329a611ad339bb578df29940b980e1858
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-08 01:02:02 +0600

    operator

--- 3. Conditional Logic/4.LeapYear.kt
+++ 3. Conditional Logic/4.LeapYear.kt
@@ -1,1 +1,20 @@
 
+
+
+fun main(args: Array<String>) {
+    var year: Int = 2004
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                println("Leap Year")
+            } else {
+                println("Not a Leap Year")
+            }
+        } else {
+            println("Leap Year")
+        }
+    } else {
+        println("Not a leap year")
+    }
+}
