e844fe760ef62b8466aba8d0229da6d1c9edfd90
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-07 14:40:48 +0600

    input output

--- 1. Input Output/1.Intro.kt
+++ 1. Input Output/1.Intro.kt
@@ -1,5 +1,17 @@
 
-
+/*
+* this is kotlin main function like java main
+* kotlin function start with fun keyword
+* unlike java kotlin language argument label comes first then the data type (like swift)
+*
+* like swift, kotlin function return type comes after the argument list
+* example
+*   fun add(a: Int, b: Int): Int {
+*       return a + b
+*   }
+* */
 fun main(args: Array<String>) {
+    // Like system.out.println()
+    // kotlin has println
     println("Hello World!")
 }
