258f524f65d2b783a3ef067a9bd616935b6bf4b5
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-08 00:48:31 +0600

    operator

---
+++ 3. Conditional Logic/1.IF.kt
@@ -0,0 +1,12 @@
+
+
+fun main(args: Array<String>) {
+    var numOne: Int = 8
+    var numTwo: Int = 12
+
+    if (numOne > numTwo) {
+        println("$numOne is the largest")
+    } else {
+        println("$numTwo is the largest")
+    }
+}
