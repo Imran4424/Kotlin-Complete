73a2addb628f07b728af2276cd1e601a7fd9d946
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-08 01:28:59 +0600

    conditional logic

---
+++ 3. Conditional Logic/8.When Digit.kt
@@ -0,0 +1,20 @@
+
+
+fun main(args: Array<String>) {
+    var digit = 7
+
+    when (digit) {
+        0 -> println("Zero")
+        1 -> println("One")
+        2 -> println("Two")
+        3 -> println("Three")
+        4 -> println("Four")
+        5 -> println("Five")
+        6 -> println("Six")
+        7 -> println("Seven")
+        8 -> println("Eight")
+        9 -> println("Nine")
+        else -> println("Not a digit")
+    }
+
+}
