5537790577e97c96f464bca309992ecd678b3033
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-08 00:56:26 +0600

    operator

---
+++ 3. Conditional Logic/3.IF Ladder.kt
@@ -0,0 +1,21 @@
+
+
+fun main(args: Array<String>) {
+    val marks = 95
+
+    if (marks >= 80) {
+        println("A+")
+    } else if (marks >= 70) {
+        println("A")
+    } else if (marks >= 60) {
+        println("B")
+    } else if (marks >= 50) {
+        println("C")
+    } else if (marks >= 45) {
+        println("D")
+    } else if(marks >= 40) {
+        println("E")
+    } else {
+        println("F")
+    }
+}
