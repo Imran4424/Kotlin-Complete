e4dd9635f98eda54650243d7ef0d3f4ee7b9c0cf
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-07 15:42:36 +0600

    input output

---
+++ 1. Input Output/2.DataType Int.kt
@@ -0,0 +1,22 @@
+
+
+fun main(args: Array<String>) {
+    // Integer type variable declaration
+
+    // variable type is Int
+    // variable type is determined by initialization value
+    var number = 10
+
+    var anotherNumber: Int
+
+    anotherNumber = 20
+
+    // string interpolation using concatenation
+    println("The integer values are " + (number) + ", " + (anotherNumber))
+
+    // string interpolation using $ sign
+    // this is much user friendly
+    println("The integer values are $number, $anotherNumber")
+
+
+}
