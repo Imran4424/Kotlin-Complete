64b449105ddf3347db7cd3cce12bfc3496e283ea
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-08 00:25:54 +0600

    operator

--- 2.Operators/Range Operators/1.DownTo.kt
+++ 2.Operators/Range Operators/1.DownTo.kt
@@ -10,6 +10,9 @@ fun main(args: Array<String>) {
     // this range contains values 9 7 5 3 2 1
     var rangeThree = 9 downTo 1 step 2
 
+    // this range contains values 10 9 8 7 6 5 4 3 2 1
+    var countDown = 10.downTo(1)
+
     // we can also use in operator for searching a value in a range
     var isPresent:Boolean = 4 in rangeTwo
     println("Is 4 present in rangeTwo? $isPresent")
