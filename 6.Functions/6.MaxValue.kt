f4addb23bea7cce7b846b9adbdaa32077461fe80
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-09 01:19:54 +0600

    functions

---
+++ 6.Functions/6.MaxValue.kt
@@ -0,0 +1,17 @@
+
+/*
+* function
+* no parameters and no return type
+* */
+
+fun maxValue(x: Int, y: Int): Int {
+    if (x > y) {
+        return x
+    } else {
+        return y
+    }
+}
+
+fun main(args: Array<String>) {
+    println("The largest number is: ${maxValue(4, 8)}")
+}
