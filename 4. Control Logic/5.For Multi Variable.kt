4e357528ca12740fb2a169e8eb844820050c555b
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-08 21:05:59 +0600

    control logic

--- 4. Control Logic/5.For Multi Variable.kt
+++ 4. Control Logic/5.For Multi Variable.kt
@@ -1,12 +1,11 @@
 // 1.2.3 + 2.3.4 + 3.4.5 + ... + n(n+1)(n+2)
 
 fun main(args: Array<String>) {
-    // FOR loop
-
     // taking int input from console
     val number: Int = readLine()!!.toInt()
     var sum: Int = 0
 
+    // here second variable j becomes pair because of second nested zip
     for ( (i, jPair) in (1..number).zip((0..number + 1).zip(2..number + 2))) {
         println("values are: $i, ${jPair.first} ${jPair.second}")
 
