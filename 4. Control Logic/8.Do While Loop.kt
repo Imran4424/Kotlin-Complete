3ecaff145ebf0881e0f21519b1c962c43e12ff8b
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-08 21:49:59 +0600

    control logic

---
+++ 4. Control Logic/8.Do While Loop.kt
@@ -0,0 +1,15 @@
+
+
+fun main(args: Array<String>) {
+    // taking int input from console
+    val number: Int = readLine()!!.toInt()
+
+    // DO WHILE LOOP
+    // kotlin do while loop is similar to java do while loop
+
+    var i: Int = 1
+    do {
+        println(i)
+        i++
+    }while (i <= number)
+}
