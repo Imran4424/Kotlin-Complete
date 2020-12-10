111f0ff0995b21025e8308ef72285c5741a789a9
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-10 23:31:42 +0600

    class

---
+++ 8.Class/4.PropertyInit.kt
@@ -0,0 +1,78 @@
+
+
+/*
+* Class
+* */
+
+
+fun main(args: Array<String>) {
+    // class variable declaration
+    var box: Box = Box(10, 12, 20)
+
+    /*
+    * the following lines will access the property of the class not parameters
+    * */
+    box.length = 2
+    box.width = 2
+    box.height = 2
+
+    println("Box area is: ${box.area()}")
+    println("Box volume is: ${box.volume()}")
+}
+
+// class declaration
+/*
+* Here, we are passing some parameters with Box class name ( i hate this part of the language)
+* this is call primary constructor
+*
+* passing parameters through primary can only be accessible through init
+* if we try to access the parameters from other parts of the class that will show error
+* */
+
+class Box(length: Int, width: Int, height: Int) {
+    // class property declaration
+    var length: Int = 0
+    var width: Int = 0
+    var height: Int = 0
+
+    // init function can access both property and constructor parameter
+    init {
+        // the following code will generate error
+        // cause compiler is accessing primary constructor parameters
+        // primary constructor parameters are by default constant
+        // that's why it's showing error in case of re assign
+        length = length
+        width = width
+        height = height
+
+        // accessing constructor parameter values
+        println("Length $length")
+        println("Width $width")
+        println("Height $height")
+    }
+
+    // class method declaration
+    fun area(): Int {
+        /*
+        * the following lines will show the property value of the class not parameters
+        * */
+        println("---area called---")
+        println("Length $length")
+        println("Width $width")
+
+        return  length * width
+    }
+
+    // class method declaration
+    fun volume(): Int {
+        /*
+        * the following lines will show the property value of the class not parameters
+        * */
+        println("---volume called---")
+        println("Length $length")
+        println("Width $width")
+        println("Height $height")
+
+        return  length * width * height
+    }
+}
