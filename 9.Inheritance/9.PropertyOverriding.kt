b55a1c61560cbb9777fe3081c9f8a4ef575242fb
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-12 15:37:35 +0600

    inheritance

---
+++ 9.Inheritance/9.PropertyOverriding.kt
@@ -0,0 +1,64 @@
+
+
+/*
+* Inheritance
+* */
+
+
+fun main(args: Array<String>) {
+    var dog = Dog()
+
+    dog.breed = "Aslacian"
+    dog.bark()
+    println(dog.color)
+    dog.eat("Bread")
+}
+
+/*
+* following error gone by making the Animal class open
+* This type is final, so it cannot be inherited from
+*
+* by default kotlin classes are public & final
+* to make a class inheritable, we need to use open
+* */
+open class Animal {
+    open var color: String = "White"
+
+    open fun eat(food: String) {
+        println("Animal Eating $food")
+    }
+}
+
+/*
+* changing it to constructor invocation the following error is gone
+* This type has a constructor, and thus must be initialized here
+* */
+class Dog: Animal() {
+    var breed:String = ""
+    override var color: String = "Black and White"
+
+    fun bark() {
+        println("Bark")
+    }
+
+    override fun eat(food: String) {
+        super<Animal>.eat(food)
+        println("Dog Eating $food")
+    }
+}
+
+/*
+* changing it to constructor invocation the following error is gone
+* This type has a constructor, and thus must be initialized here
+* */
+class Cat: Animal() {
+    var age: Int = 0
+
+    fun meow() {
+        println("Meow")
+    }
+
+    override fun eat(food: String) {
+        println("Dog Eating $food")
+    }
+}
